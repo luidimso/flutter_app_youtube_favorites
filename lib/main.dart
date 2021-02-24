9abf6ec93c8f2e5632ee13fe71f0a1233eb4a04a
luidimso <luidi_mso@outlook.com>
2021-02-24 11:41:43 -0300

    Search videos endpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_youtube_favorites/pages/api_page.dart';
 import 'package:flutter_app_youtube_favorites/pages/home_page.dart';
 
-class MyApp extends StatelessWidget {
+void main() {
+  Youtube api = Youtube();
+  api.search("Flog Leap");
 
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
--- lib/pages/api_page.dart
+++ lib/pages/api_page.dart
@@ -1,1 +1,28 @@
+import 'dart:convert';
+
+import 'package:flutter_app_youtube_favorites/models/video.dart';
+import 'package:http/http.dart' as http;
+
 const API_KEY = "AIzaSyC1oiBaxRLPWeSDEmkQLG6BMEg8Cqwox0g";
+
+class Youtube {
+  search(String search) async {
+    http.Response response = await http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10");
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+    if(response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+      List<Video> videos = decoded["items"].map<Video>(
+          (map) {
+            return Video.fromJson(map);
+          }
+      ).toList();
+
+      return videos;
+    } else {
+      throw Exception("Failed to load videos");
+    }
+  }
+}

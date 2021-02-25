3c8e672e12c210d011117f86c3b32a487d05c9ef
luidimso <luidi_mso@outlook.com>
2021-02-25 10:41:10 -0300

    App bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/youtube-logo.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+              icon: Icon(Icons.star),
+              onPressed: () {}
+          ),
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {}
+          )
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
